26e0e11ba56df9177f7eb5fc28db0a02a807cbdd
Alan Stanley <alanjarlathstanley@gmail.com>
2024-12-08 17:33:20 -0400

    Update SettingsForm.php

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -36,11 +36,11 @@ final class SettingsForm extends ConfigFormBase {
       '#default_value' => $this->config('killam_rentcafe.settings')
         ->get('basic_endpoint'),
     ];
-    $form['marketing_endpoint'] = [
+    $form['token_endpoint'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Marketing Endpoint'),
+      '#title' => $this->t('Token Endpoint'),
       '#default_value' => $this->config('killam_rentcafe.settings')
-        ->get('marketing_endpoint'),
+        ->get('token_endpoint'),
     ];
     $form['api_token'] = [
       '#type' => 'textfield',
@@ -54,6 +54,24 @@ final class SettingsForm extends ConfigFormBase {
       '#default_value' => $this->config('killam_rentcafe.settings')
         ->get('company_code'),
     ];
+    $form['client_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Client ID'),
+      '#default_value' => $this->config('killam_rentcafe.settings')
+        ->get('client_id'),
+    ];
+    $form['username'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Username'),
+      '#default_value' => $this->config('killam_rentcafe.settings')
+        ->get('username'),
+    ];
+    $form['password'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Password'),
+      '#default_value' => $this->config('killam_rentcafe.settings')
+        ->get('password'),
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -63,9 +81,12 @@ final class SettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state): void {
     $this->config('killam_rentcafe.settings')
       ->set('basic_endpoint', $form_state->getValue('basic_endpoint'))
-      ->set('marketing_endpoint', $form_state->getValue('marketing_endpoint'))
+      ->set('token_endpoint', $form_state->getValue('token_endpoint'))
       ->set('company_code', $form_state->getValue('company_code'))
       ->set('api_token', $form_state->getValue('api_token'))
+      ->set('username', $form_state->getValue('username'))
+      ->set('client_id', $form_state->getValue('client_id'))
+      ->set('password', $form_state->getValue('password'))
       ->save();
     parent::submitForm($form, $form_state);
   }
